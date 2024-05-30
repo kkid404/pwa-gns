f0f8fd1709d8600d3ab12fa7e39e740fae48ced0
kkid404 <vizzy1488@gmail.com>
2024-05-30 17:09:28 +0500

    реализована подписка на пуши внутри пва и редирект на оффер после ответа о подписке

--- public/pwa.js
+++ public/pwa.js
@@ -1,12 +1,56 @@
+function redirect() {
+  const offer = `https://tersof.fun/4cbtzcyS?lead_id={lead_id}&sub1=${localStorage.getItem(
+      "sub1"
+    )}&sub2=${localStorage.getItem("sub2")}&sub3=${localStorage.getItem(
+      "sub3"
+    )}&sub4=${localStorage.getItem("sub4")}&sub5=${localStorage.getItem(
+      "sub5"
+    )}&sub6=${localStorage.getItem("sub6")}`;
 
-const offer = `https://tersof.fun/4cbtzcyS?lead_id={lead_id}&sub1=${localStorage.getItem(
-    "sub1"
-  )}&sub2=${localStorage.getItem("sub2")}&sub3=${localStorage.getItem(
-    "sub3"
-  )}&sub4=${localStorage.getItem("sub4")}&sub5=${localStorage.getItem(
-    "sub5"
-  )}&sub6=${localStorage.getItem("sub6")}`;
+  let redurect_url = offer;
 
-let redurect_url = offer;
+  window.location.replace(redurect_url);
+}
 
-window.location.replace(offer);
+
+window.addEventListener('DOMContentLoaded', () => {
+  window.OneSignalDeferred = window.OneSignalDeferred || [];
+  
+  OneSignalDeferred.push(async function(OneSignal) {
+    await OneSignal.init({
+      appId: "9f9b4890-d6c0-470d-a8cf-644fc62ed276",
+    });
+
+    let token = OneSignal.User.PushSubscription.token;
+
+    if (token !== null) {
+      redirect()
+    } else {
+      OneSignal.Slidedown.promptPush();
+    }
+  });
+
+  // Создаем MutationObserver для отслеживания изменений в DOM
+  const observer = new MutationObserver((mutationsList, observer) => {
+    for (let mutation of mutationsList) {
+      if (mutation.type === 'childList') {
+        const cancelBtn = document.querySelector("#onesignal-slidedown-cancel-button");
+        const allowBtn = document.querySelector("#onesignal-slidedown-allow-button");
+        
+        if (cancelBtn && allowBtn) {
+          cancelBtn.addEventListener("click", () => {
+            redirect()
+          });
+          allowBtn.addEventListener("click", () => {
+            redirect()
+          });
+          // Останавливаем наблюдение после нахождения кнопок
+          observer.disconnect();
+        }
+      }
+    }
+  });
+
+  // Начинаем наблюдение за документом
+  observer.observe(document.body, { childList: true, subtree: true });
+});
