c0b137c477eeb478022f070baacacb7d465e2645
kkid404 <vizzy1488@gmail.com>
2024-07-18 21:11:27 +0500

    фикс работы на пк

--- src/App.tsx
+++ src/App.tsx
@@ -4,21 +4,30 @@ import InstallPage from './components/InstallPage';
 import PWAPage from './components/PWAPage';
 
 const App: React.FC = () => {
-  const [isPWA, setIsPWA] = useState(false);
+  const [isPWA, setIsPWA] = useState<boolean | null>(null);
 
   useEffect(() => {
     // Определение, является ли приложение PWA
-    const isPWA = window.matchMedia('(display-mode: standalone)').matches;
-    setIsPWA(isPWA);
+    const checkPWA = () => {
+      const isPWA = window.matchMedia('(display-mode: standalone)').matches;
+      setIsPWA(isPWA);
+    };
+
+    // Добавляем небольшую задержку для демонстрации загрузочного экрана
+    setTimeout(checkPWA, 100);
   }, []);
+
+  if (isPWA === null) {
+    return (
+      <div className="loader-wrapper">
+        <span className="loader"></span>
+      </div>
+    );
+  }
 
   return (
     <>
-      {isPWA ? (
-        <PWAPage />
-      ) : (
-        <InstallPage />
-      )}
+      {isPWA ? <PWAPage /> : <InstallPage />}
     </>
   );
 };
